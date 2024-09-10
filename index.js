7eac5876449e129f4e45504a37742c13240f6a7c
Karen-Victor <karenyvictor14102023@gmail.com>
2024-09-09 23:49:35 -0500

    Versiones de archivos cambiados

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function Init(){
         try {
             if(consultando) return;
             consultando = true;
-            // const peticion = await fetch('https://github.com/Karen-Victor/Sorteo-Boda/blob/main/numeros-vendidos.json?v=2');
-            // const numerosVendidos = await peticion.json();
-            numerosVendidos = [0,1,4,6,7,8,9,10,14,16,21,22,23,26,27,29,30,36,38,43,57,62,75,78,82,83,87,92,96,98];
+            const peticion = await fetch('https://github.com/Karen-Victor/Sorteo-Boda/blob/main/numeros-vendidos.json?v=3');
+            const numerosVendidos = await peticion.json();
             let html = ``;
             for(let i=0; i<100; i++){
                 const clase = numerosVendidos.includes(i) ? ' class="sin-vender"' : '';
