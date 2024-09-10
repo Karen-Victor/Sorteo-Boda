ed509f1c325be0381aff1103ac48dcabe37a0ade
Karen-Victor <karenyvictor14102023@gmail.com>
2024-09-09 21:43:47 -0500

    Archivos enlazados

---
+++ index.js
@@ -0,0 +1,11 @@
+function Init(){
+    const numerosVendidos = [0,1,4,6,7,8,9,10,14,16,21,22,23,26,27,29,30,36,38,43,57,62,75,78,82,83,87,96,98]
+    let html = ``;
+    for(let i=0; i<100; i++){
+        const clase = numerosVendidos.includes(i) ? 'vendida' : 'sin-vender';
+        html += `<span class="clase">${i}</span>`;
+    }
+
+    tabla.innerHTML = html;
+}
+Init();
