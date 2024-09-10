21c3fdfce10b45839182e5292fe3111760692728
Karen-Victor <karenyvictor14102023@gmail.com>
2024-09-09 22:19:19 -0500

    Diseño aplicado

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ function Init(){
     let consultando = false;
     setInterval(async ()=>{
         try {
-            const peticion = await fetch('https://github.com/Karen-Victor/Sorteo-Boda/blob/main/numeros-vendidos.json');
-            const datos = await peticion.json();
-            console.log(datos);
-            // if(consultando) return;
-            // consultando = true;
-            // const numerosVendidos = [0,1,4,6,7,8,9,10,14,16,21,22,23,26,27,29,30,36,38,43,57,62,75,78,82,83,87,96,98]
-            // let html = ``;
-            // for(let i=0; i<100; i++){
-            //     const clase = numerosVendidos.includes(i) ? 'vendida' : 'sin-vender';
-            //     html += `<span class="clase">${i}</span>`;
-            // }
-            // tabla.innerHTML = html;
+            if(consultando) return;
+            consultando = true;
+            // const peticion = await fetch('https://github.com/Karen-Victor/Sorteo-Boda/blob/main/numeros-vendidos.json');
+            // const numerosVendidos = await peticion.json();
+            // console.log(numerosVendidos);
+
+            const numerosVendidos = [0,1,4,6,7,8,9,10,14,16,21,22,23,26,27,29,30,36,38,43,57,62,75,78,82,83,87,96,98]
+            let html = ``;
+            for(let i=0; i<100; i++){
+                const clase = numerosVendidos.includes(i) ? ' class="vendido"' : '';
+                html += `<span ${clase}>${i}</span>`;
+            }
+            tabla.innerHTML = html;
         } catch (error) {
             consultando = false;
         }
        
-    },2000);
+    },1000);
     
 }
 Init();
