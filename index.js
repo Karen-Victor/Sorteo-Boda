3e7d61346419a3650a2455b9cd593e50a2864fd5
Karen-Victor <karenyvictor14102023@gmail.com>
2024-09-10 01:44:26 -0500

    Version 10

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 function Init(){
+    function calcularTiempoRestante() {
+        const ahora = new Date();
+        const fechaObjetivo = new Date('2024-10-12T00:00:00');
+        const diferencia = fechaObjetivo - ahora;
+        if(diferencia>0){
+            const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+            const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+            const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+            spanTemporizador.innerText = `Faltan ${dias} días, ${horas} horas, ${minutos} minutos y ${segundos} segundos!`;
+        }else {
+            spanTemporizador.innerText = `Hoy juega el sorteo con los últimos 2 números de la lotería Boyacá!`;
+        }
+        
+    }
+    
+    calcularTiempoRestante();
+    setInterval(calcularTiempoRestante,1000);
+
     let html = ``;
     for(let i=0; i<100; i++){
         html += `<span class="sin-vender" id="span${i}">${i}</span>`;
