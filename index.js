c52c2ad68eb4fbd362039dbcb1923a4d0838dc25
Karen-Victor <karenyvictor14102023@gmail.com>
2024-09-09 23:48:35 -0500

    Diseño terminado

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ function Init(){
         try {
             if(consultando) return;
             consultando = true;
-            // const peticion = await fetch('https://github.com/Karen-Victor/Sorteo-Boda/blob/main/numeros-vendidos.json');
+            // const peticion = await fetch('https://github.com/Karen-Victor/Sorteo-Boda/blob/main/numeros-vendidos.json?v=2');
             // const numerosVendidos = await peticion.json();
-            // console.log(numerosVendidos);
-
-            const numerosVendidos = [0,1,4,6,7,8,9,10,14,16,21,22,23,26,27,29,30,36,38,43,57,62,75,78,82,83,87,96,98]
+            numerosVendidos = [0,1,4,6,7,8,9,10,14,16,21,22,23,26,27,29,30,36,38,43,57,62,75,78,82,83,87,92,96,98];
             let html = ``;
             for(let i=0; i<100; i++){
-                const clase = numerosVendidos.includes(i) ? ' class="vendido"' : '';
+                const clase = numerosVendidos.includes(i) ? ' class="sin-vender"' : '';
                 html += `<span ${clase}>${i}</span>`;
             }
             tabla.innerHTML = html;
         } catch (error) {
             consultando = false;
         }
-       
     },1000);
-    
 }
 Init();
